4e41d057a3f34acbc6737664e04916b6226a431b
Andreas Claesson <andreas.claesson@sogeti.se>
2014-09-17 18:50:09 +0200

    Added text view to SecondViewController that will show activity log on viewWillAppear

--- NSSpainEnteranceTracker/SecondViewController.swift
+++ NSSpainEnteranceTracker/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var loggerTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
 }
 
