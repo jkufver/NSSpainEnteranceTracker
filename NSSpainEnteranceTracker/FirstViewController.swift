10025a3f6092cbefae7c635eb1a5e5075dbf66e1
Jens Kufver <jens.kufver@sogeti.se>
2014-09-17 11:45:02 +0200

    Initial Commit

---
+++ NSSpainEnteranceTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NSSpainEnteranceTracker
+//
+//  Created by Jens Kufver on 2014-09-17.
+//  Copyright (c) 2014 Jens Kufver. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NSSpainEnteranceTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NSSpainEnteranceTracker
+//
+//  Created by Jens Kufver on 2014-09-17.
+//  Copyright (c) 2014 Jens Kufver. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
